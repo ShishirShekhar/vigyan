596ab4cb84d7cf7b223baad015efcb9e5927ed48
ShishirShekhar <sspdav02@gmail.com>
2023-10-18 22:27:20 +0530

    add doppler route

--- index.js
+++ index.js
@@ -61,17 +61,36 @@ app.post("/api/v1/thermal/:values", async (req, res) => {
   }
 });
 
-app.get("/api/v1/thermal", async (req, res) => {
+app.get("/api/v1/doppler", async (req, res) => {
   try {
     const auth = await getAuthToken();
     const sheet = await getSpreadSheetValues({
       spreadsheetId: spreadsheetId,
       auth: auth,
-      sheetName: "thermal!A:ZZ",
+      sheetName: "doppler!A:ZZ",
     });
     const values = sheet.data.values;
 
     res.status(200).json(values);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error });
+  }
+});
+
+app.post("/api/v1/doppler/:values", async (req, res) => {
+  try {
+    const auth = await getAuthToken();
+    const update = await updateSpreadSheetValues({
+      spreadsheetId: spreadsheetId,
+      auth: auth,
+      sheetName: "doppler!A:ZZ",
+      values: JSON.parse(req.params.values),
+    });
+
+    update
+      ? res.status(200).json(update)
+      : res.status(500).json({ message: "Error while updating" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: error });
